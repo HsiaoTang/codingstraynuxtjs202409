e22df947d05fa61066832bd84c89a79156678242
HsiaoTang <103110828+HsiaoTang@users.noreply.github.com>
2025-01-23 17:22:10 +0800

    topic grids

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,64 @@
 <template>
-  <TabContol />
+  <TabContol :tabFuncs="tabFuncs"/>
 </template>
 
 <script setup lang="ts">
 import { definePageMeta } from '~/node_modules/nuxt/dist/pages/runtime/composables';
-import TabContol from '~/components/common/TabContol.vue';
+import { LatestSvg, PopularSvg, TopicsSvg } from '~/assets/icons';
+import { TabContol, TopicGrids, PopularGrids, LatestGrids } from '~/components/common';
+
 
 definePageMeta({ layout: 'entrypoint' })
+
+
+const tabFuncs = [
+  {
+    title: 'topics',
+    icon: TopicsSvg,
+    component: TopicGrids,
+    content: {
+      topicGridPropsArray: [
+        {
+          title: 'art',
+          url: '/imgs/topics/artPic.jpg',
+        },
+        {
+          title: 'coding',
+          url: '/imgs/topics/codingPic.jpg',
+        },
+        {
+          title: 'music',
+          url: '/imgs/topics/musicPic.jpg',
+        },
+        {
+          title: 'nature',
+          url: '/imgs/topics/naturePic.jpg',
+        },
+        {
+          title: 'politics',
+          url: '/imgs/topics/politicsPic.jpg',
+        },
+        {
+          title: 'sport',
+          url: '/imgs/topics/sportPic.jpg',
+        }
+      ]
+    }
+  },
+  {
+    title: 'popular',
+    icon: PopularSvg,
+    component: PopularGrids,
+    content: []
+  },
+  {
+    title: 'latest',
+    icon: LatestSvg,
+    component: LatestGrids,
+    content: []
+  },
+];
+
+
 
 </script>
